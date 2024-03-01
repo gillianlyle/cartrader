64729e0f6028c0d01f5b46ea5fb6097a75492330
Gillian Lyle <gillian.lyle@outlook.com>
2024-03-01 12:21:34 +0000

    Section 2: file based routing and navigation

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam magnam explicabo veritatis omnis tempore debitis eveniet laborum voluptate, expedita esse iste quo iusto incidunt accusantium rerum culpa porro, consequatur adipisci!
+    </p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+    useHead({
+        title: "Cartrader"
+    })
+</script>
+
+<template>
+    <div>
+        <NavBar />
+        <CarHero />
+    </div>
+</template>
