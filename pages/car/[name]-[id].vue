d7d9854a0da8346d4c1702f5c3201445cde0cc05
Gillian Lyle <gillian.lyle@outlook.com>
2024-03-01 15:21:13 +0000

    Section 3: defining page layouts

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,18 +1,21 @@
 <script setup>
   const route = useRoute();
+  const { toTitleCase } = useUtilities();
+
   useHead({
-    title: route.params.name
+    title: toTitleCase(route.params.name)
+  })
+
+  definePageMeta({
+    layout: "custom"
   })
 </script>
 
 <template>
-    <NavBar />
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-      <div>
-        <CarDetailHero />
-        <CarDetailAttributes />
-        <CarDetailDescription />
-        <CarDetailContact />
-    </div>
+  <div>
+    <CarDetailHero />
+    <CarDetailAttributes />
+    <CarDetailDescription />
+    <CarDetailContact />
   </div>
 </template>
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,19 +1,21 @@
 <script setup>
     const route = useRoute();
+    const { toTitleCase } = useUtilities();
+    
     useHead({
-        title: `${route.params.make ? route.params.make : 'Cars'} in ${route.params.city}`,
+        title: `${route.params.make ? toTitleCase(route.params.make) : 'Cars'} in ${toTitleCase(route.params.city)}`,
+    })
+
+    definePageMeta({
+        layout: 'custom'
     })
 </script>
 
 <template>
     <div>
-       <NavBar />
-
-       <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
         <div class="mt-32 flex">
             <CarSideBar />
             <NuxtPage />
-        </div>
         </div>
     </div>
 </template>
